a91a19551fbc09c37aff5da14656d70847356483
Your Name <phutakvinaya@gmail.com>
2023-12-31 10:10:49 +0530

    initial commit

---
+++ assignment7.3/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'assignment7.3';
+  text1 :boolean= false;
+
+  public fun(){
+    this.text1= !this.text1;
+  }
+}
